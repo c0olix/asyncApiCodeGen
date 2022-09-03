44b601939c90d16633a46d3cda5d6816b55f2173
Janek Stoeck <janek.stoeck@akra.de>
2022-09-03 15:01:20 +0200

    fix: remove example output, as it uses a private repository and breaks tests

--- generator/test-spec/expected/out/IUserLockedEventProducer.java
+++
@@ -1,7 +0,0 @@
-package de.hochbahn.events;
-
-import javax.validation.constraints.NotNull;
-
-public interface IUserLockedEventProducer {
-    void produceUserLockedEvent(@NotNull UserLockedEvent userLockedEvent);
-}
